41990e83c3cb2417b6ef356df4376e319b967f35
dile <duyle131103@gmail.com>
2025-03-28 23:35:15 +0700

    feat: add header and footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,18 @@ import { Routes } from '@angular/router';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
   {
     path: 'sign-up',
     component: SignUpComponent,
@@ -11,11 +21,6 @@ export const routes: Routes = [
   {
     path: 'sign-in',
     component: SignInComponent,
-  },
-  {
-    path: '',
-    redirectTo: '/sign-up',
-    pathMatch: 'full',
   },
   {
     path: '**',
