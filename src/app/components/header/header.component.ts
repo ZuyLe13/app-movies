7ab5b8352ed6849c24a2ab1e8f62f63952c4a514
dile <duyle131103@gmail.com>
2025-03-29 23:54:18 +0700

    feat: update header & footer style

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
